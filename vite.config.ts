0402e9f9b06143f06f7823468117afa761397981
pansong291 <pansong291@sina.com>
2025-09-25 16:49:46 +0800

    ci: 打包结果页使用别名

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,21 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import { readdirSync, statSync } from 'node:fs'
 
+type Entry = {
+  alias?: string
+  key: string
+  path: string
+}
+
+// 打包结果优先使用别名，开发的时候两者都可以
+const entryAlias: Record<string, string> = {
+  'sky-fengxu': 'a5ebbee14749'
+}
+
 // 动态获取多页面入口函数
-const multiPageInput = (() => {
+const entryRules = (() => {
   const baseDir = resolve(__dirname, 'src/pages')
-  const entries: Record<string, any> = {
-    // 主入口
-    index: resolve(__dirname, 'index.html')
-  }
+  const entries: Entry[] = [{ key: 'index', path: resolve(__dirname, 'index.html') }]
   try {
     // 遍历 pages 目录
     const items = readdirSync(baseDir)
@@ -21,7 +29,7 @@ const multiPageInput = (() => {
         try {
           // 检查 index.html 是否存在
           statSync(htmlPath)
-          entries[item] = htmlPath // 使用文件夹名作为入口名
+          entries.push({ alias: entryAlias[item], key: item, path: htmlPath })
         } catch (e) {
           console.warn(`No index.html found in ${itemPath}, skipping.`)
         }
@@ -33,13 +41,18 @@ const multiPageInput = (() => {
   return entries
 })()
 
+const entryInput = entryRules.reduce((p, v) => {
+  p[v.alias || v.key] = v.path
+  return p
+}, {})
+
 // 用于开发模式下路径重写的自定义插件
 function mpaHistoryFallbackPlugin(): PluginOption {
-  const rules = Object.entries(multiPageInput)
-  for (const rule of rules) {
-    rule[0] = '/' + rule[0]
+  for (const rule of entryRules) {
+    if (rule.alias) rule.alias = '/' + rule.alias
+    rule.key = '/' + rule.key
     // 将本地的绝对路径替换为 url 的相对路径
-    rule[1] = rule[1].replace(__dirname, '').replaceAll('\\', '/')
+    rule.path = rule.path.replace(__dirname, '').replaceAll('\\', '/')
   }
 
   return {
@@ -48,13 +61,17 @@ function mpaHistoryFallbackPlugin(): PluginOption {
     configureServer(server) {
       // 添加一个中间件到 Vite 开发服务器
       server.middlewares.use((req, res, next) => {
-        for (const rule of rules) {
-          if (req.url === rule[0]) {
+        for (const rule of entryRules) {
+          const originUrl = req.url
+          if (originUrl.startsWith(rule.alias)) {
+            req.url = originUrl.replace(rule.alias, rule.key)
+          }
+          if (req.url === rule.key) {
             // 当访问无斜杠结尾的目录时，需要补充斜杠并引导浏览器重定向
-            res.writeHead(301, { Location: req.url + '/' })
+            res.writeHead(301, { Location: originUrl + '/' })
             res.end()
             return
-          } else if (req.url.startsWith(rule[0])) {
+          } else if (req.url.startsWith(rule.key)) {
             req.url = '/src/pages' + req.url // 重写请求的 URL
             break
           }
@@ -67,6 +84,8 @@ function mpaHistoryFallbackPlugin(): PluginOption {
 
 // 处理打包后的目录结构
 function flattenOutput(): PluginOption {
+  const reg = new RegExp('src/pages/([^/]+)')
+
   return {
     name: 'vite-plugin-flatten-output',
     apply: 'build',
@@ -74,8 +93,13 @@ function flattenOutput(): PluginOption {
     generateBundle(_, bundle) {
       for (const output of Object.values(bundle)) {
         if (typeof output === 'object' && output.fileName?.endsWith('.html')) {
-          // 去掉 pages 目录层级
-          output.fileName = output.fileName.replace('src/pages/', '')
+          const exeArr = reg.exec(output.fileName)
+          const folder = exeArr?.[1]
+          const alias = entryAlias[folder]
+          if (folder && alias) {
+            // 去掉 pages 目录层级并替换为别名
+            output.fileName = output.fileName.replace(exeArr[0], alias)
+          }
         }
       }
     }
@@ -99,7 +123,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      input: multiPageInput,
+      input: entryInput,
       output: {
         entryFileNames: (chunkInfo) => {
           if (chunkInfo.isEntry) return '[name]/[hash].js'
